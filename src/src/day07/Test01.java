4a1bd8e954a584967dc22298c6f5fead59fac9dd
Sangwon-Shin <112858114+Sangwon-Shin@users.noreply.github.com>
2022-10-20 18:51:25 +0900

    Add files via upload

---
+++ src/src/day07/Test01.java
@@ -0,0 +1,55 @@
+package day07;
+
+/*
+	잘못된 switch ~ case 사용 예
+ */
+
+public class Test01 {
+
+	public static void main(String[] args) {
+		// 랜덤하게 정수 발생
+		int no = (int)(Math.random()*10);
+		
+		// 결과값 변수 
+		String result = "";
+		
+		// switch ~ case 처리
+		switch(no) {
+		case	0:
+			System.out.println(no);
+			result = "영";
+			break;
+		case	1:
+			System.out.println(no);
+			result = "하나";
+			break;
+		case	2:
+			System.out.println(no);
+			result = "둘";
+			break;
+		case	3:
+			System.out.println(no);
+			result = "셋";
+		case	4:
+			System.out.println(no);
+			result = "넷";
+		case	5:
+			System.out.println(no);
+			result = "다섯";
+		case	6:
+			System.out.println(no);
+			result = "여섯";
+		case	7:
+			System.out.println(no);
+			result = "일곱";
+		case	8:
+			System.out.println(no);
+			result = "여덟";
+		case	9:
+			System.out.println(no);
+			result = "아홉";
+		}
+		System.out.println("입력된 정수는 [ " + no + " ] 이고 [ "+ result + " ] 입니다.");
+	}
+
+}
---
+++ src/src/day07/Test07.java
@@ -0,0 +1,35 @@
+package day07;
+
+public class Test07 {
+
+	public static void main(String[] args) {
+		MyInfo jennie = new MyInfo();
+		jennie.name = "jennie";
+		jennie.height = 165.5;
+		jennie.gen = 'F';
+		
+		System.out.println("name : " + jennie.name);
+		System.out.println("height : " + jennie.height);
+		System.out.println("gen : " + jennie.gen);
+		
+		MyInfo euns = new MyInfo();
+		euns.name = "전은석";
+		System.out.println("name : " + euns.name);
+		System.out.println();
+		
+		
+		MyInfo[] member = new MyInfo[4];
+		for(int i = 0 ; i < 4 ; i++ ) {
+			member[i] = new MyInfo();
+		}
+		member[0].name = "jennie";
+		member[1].name = "lisa";
+		member[2].name = "rose";
+		member[3].name = "jisoo";
+		
+		for(MyInfo m : member) {
+			System.out.println(m.name);
+		}
+	}
+
+}

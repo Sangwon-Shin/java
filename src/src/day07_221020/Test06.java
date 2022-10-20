7010cd166c4649ae89d290a8e8033dca7406d066
bluer <bluer@211.54.66.72>
2022-10-20 20:16:41 +0900

    221020 20:16

--- src/src/day07_221020/Test06.java
+++ src/src/day07_221020/Test06.java
@@ -1,3 +1,6 @@
+// 자바에서 모든 클래스는 Object 클래스를 상속한다.
+// 그러나 이렇게 쓰면 안된다.
+
 package day07_221020;
 
 import java.util.Arrays;
@@ -11,7 +14,7 @@ public class Test06 {
 		obj[1] = 27;
 		obj[2] = 'F';
 		
-		obj[1] = (int)obj[1] + 1;
+		obj[1] = (int)obj[1] + 1;	// 해가 바뀌어서 나이를 +1 하려면 강제형변환을 해야한다.
 		// 저장 : int ---> Integer ---> Object
 		// 위의 연산 : Object ---> Integer ---> int
 		System.out.println(Arrays.toString(obj));
